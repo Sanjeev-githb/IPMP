2b0a45804dba543be3350988dea4e432f11cb795
Sanjeev-githb <105710798+Sanjeev-githb@users.noreply.github.com>
2023-02-11 09:39:15 +0530

    Update q21.cpp

--- week 1/q21.cpp
+++ week 1/q21.cpp
@@ -1,42 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void printPairs(int a[], int n, int x)
-{
-	int i;
-	int rem[x];
-	for (i = 0; i < x; i++)
-		rem[i] = 0;
-	for (i = 0; i < n; i++)
-		if (a[i] < x)
-			rem[a[i] % x]++;
-	for (i = 1; i < x / 2; i++) {
-		if (rem[i] > 0 && rem[x - i] > 0) {
-			cout << "Yes\n";
-			break;
+bool chkPair(int A[], int size, int x)
+{              for (int i = 0; i < (size - 1); i++) {
+		for (int j = (i + 1); j < size; j++) {
+			if (A[i] + A[j] == x) {
+				return 1;
+			}
 		}
 	}
-	if (i >= x / 2) {
-		if (x % 2 == 0) {
-			if (rem[x / 2] > 1)
-				cout << "Yes\n";
-			else
-				cout << "No\n";
-		}
-		else {
-			if (rem[x / 2] > 0 && rem[x - x / 2] > 0)
-				cout << "Yes\n";
-			else
-				cout << "No\n";
-		}
-	}
+
+	return 0;
 }
 int main()
 {
-	int A[] = { 1, 4, 45, 6, 10, 8 };
-	int n = 16;
-	int arr_size = sizeof(A) / sizeof(A[0]);
-	printPairs(A, arr_size, n);
-    return 0;
+	int A[] = { 0, -1, 2, -3, 1 };
+	int x = -2;
+	int size = sizeof(A) / sizeof(A[0]);
+
+	if (chkPair(A, size, x)) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << x << endl;
+	}
+        return 0;
 }
+
 
